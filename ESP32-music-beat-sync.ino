4d7f5f8b4d2e63c21c7b1f9bb6a7a20139b8773f
blaz-r <blaz.rolih@gmail.com>
2021-05-08 13:15:26 +0200

    Updated defines for easier tweaking of limits

--- ESP32-music-beat-sync.ino
+++ ESP32-music-beat-sync.ino
@@ -13,6 +13,37 @@
 
 #define MIC_PIN 32
 
+/*  
+ *  following are defines used for updating limits, you can adjust if you want
+ *  it's all calcualted as X < Y * define, where X is current difference, 
+ *  Y is reference value of which we want to get percentage of
+ *  example: 
+ *    #define LED_FREQ_LIM 0.65
+ *    freq < freqMax*LED_FREQ_LIM 
+ *    
+ *  means that frequency needs to be smaller than 65% of peak frequency.
+ *  following defines tell that percentage factor:
+ */
+ 
+// difference between current max magnitude and previous max magnitude needs to be greater than magMax*MAX_MAG_DIFF
+#define MAX_MAG_DIFF 0.13
+// difference between current max magnitude and current calculated magnitude needs to be greater than magMax*CURR_MAG_DIFF
+#define CURR_MAG_DIFF 0.5
+// difference between current max frequency and previous max frequency needs to be greater than freqMax*MAX_FREQ_DIFF
+#define MAX_FREQ_DIFF 0.2  
+// difference between current max frequency and current calculated frequency needs to be greater than freqMax*CURR_FREQ_DIFF
+#define CURR_FREQ_DIFF 0.7 
+
+/*
+ * following are all defines for setting limits that count as beat, calculated same as above
+ */
+// frequency needs to be lower than freqMax*LED_FREQ_LIM
+#define LED_FREQ_LIM 0.65
+// difference between current max magnitude and current calculated magnitude needs to be greater than magMax*LED_MAG_DIFF_LIM
+#define LED_MAG_DIFF_LIM 0.31
+// current magnitued needs to be greater than magMax*LED_MAG_LIM
+#define LED_MAG_LIM 0.42
+
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM, LED_PIN, NEO_GRBW + NEO_KHZ800);
 
 const uint16_t samples = 256;               // This value MUST ALWAYS be a power of 2
@@ -61,9 +92,9 @@ void analyzeMusic() {
 
   FFT.MajorPeak(&freq, &mag); // save current peak frequency and magnitued
 
-  // set magnitude and frequency
+  // update max magnitude and frequency
   magMax = max(mag, magMax);
-  // ifnore frequencies above 250, since we want bass
+  // ignore frequencies above 250, since we want bass
   if(freq < 250)
     freqMax = max(freq, freqMax);
 
@@ -73,7 +104,7 @@ void analyzeMusic() {
      * Adjust max magnitude if the difference from previus max magnitude is more than 13%
      * or if difference between max magnitude and current calcuated magnitued is more than 50%
      */
-    if(abs(magMaxPrev - magMax) > magMax*0.13 || abs(magMax - mag) > magMax*0.5) {
+    if(abs(magMaxPrev - magMax) > magMax*MAX_MAG_DIFF || abs(magMax - mag) > magMax*CURR_MAG_DIFF) {
       magMax *= 0.6; // 60% of previous value
       magMaxPrev = magMax;  // save it for further adjsutments
     }
@@ -81,7 +112,7 @@ void analyzeMusic() {
      * Adjust max frequency if the difference from previus max frequency is more than 20%
      * or if difference between max magnitude and current calcuated magnitued is more than 69%
      */
-    if(abs(freqMaxPrev - freqMax) > freqMax*0.2 || abs(freqMax - freq) > freqMax*0.69) {
+    if(abs(freqMaxPrev - freqMax) > freqMax*MAX_FREQ_DIFF || abs(freqMax - freq) > freqMax*CURR_FREQ_DIFF) {
       freqMax *= 0.8;
       freqMaxPrev = freqMax;
     }
@@ -109,7 +140,7 @@ int fade = 0; // fading of leds
  * to be at least 42% of peak magnitude
  */
 void controlLed() {
-  if(freq < freqMax*0.65 && abs(mag - prevMag) > magMax * 0.314 && mag > magMax * 0.42) {
+  if(freq < freqMax*LED_FREQ_LIM && abs(mag - prevMag) > magMax*LED_MAG_DIFF_LIM && mag > magMax*LED_MAG_LIM) {
     logData();
      
     uint32_t color = strip.Color(0, 0, 0, 100);
